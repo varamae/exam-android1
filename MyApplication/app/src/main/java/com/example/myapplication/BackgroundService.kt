6117c47c914bfc6d86ba1f5452beb006c40f9412
varamae <67048041+varamae@users.noreply.github.com>
2020-07-05 21:42:52 +0700

    Update BackgroundService.kt

--- MyApplication/app/src/main/java/com/example/myapplication/BackgroundService.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/BackgroundService.kt
@@ -61,17 +61,10 @@ class BackgroundService : Service() {
             .setSmallIcon(R.drawable.stat_notify_sync)
             .setContentTitle(title)
             .setContentText(text)
-        // Creates an explicit intent for an Activity in your app
         val resultIntent = Intent(this, MainActivity::class.java)
 
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
         val stackBuilder = TaskStackBuilder.create(this)
-        // Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(MainActivity::class.java)
-        // Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent)
         val resultPendingIntent = stackBuilder.getPendingIntent(
             0,
@@ -80,7 +73,6 @@ class BackgroundService : Service() {
         mBuilder.setContentIntent(resultPendingIntent)
         val mNotificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        // mId allows you to update the notification later on.
         mNotificationManager.notify(1, mBuilder.build())
     }
 
